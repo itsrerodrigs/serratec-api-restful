0f13177adbf05874e7476081023b34a1ce2c2ace
Lucas Coco <lucasgcocos@gmail.com>
2024-10-16 23:18:34 -0300

    Adicionando pacote de TratamentoDeErro.java e utilizando as regras de negocio em entidades

--- serratecpub/src/main/java/org/serratec/serratecpub/model/Categoria.java
+++ serratecpub/src/main/java/org/serratec/serratecpub/model/Categoria.java
@@ -1,11 +1,24 @@
 package org.serratec.serratecpub.model;
 
+import org.serratec.serratecpub.util.TratamentoDeErro;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 @Entity
 public class Categoria {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	@NotBlank(message = TratamentoDeErro.NotBlankMessage)
+	@Size(min = 3, max = 50, message = TratamentoDeErro.SizeMessage)
 	private String nome;
+	@NotBlank(message = TratamentoDeErro.NotBlankMessage)
+	@Size(max = 150, message = TratamentoDeErro.SizeMessage)
 	private String descricao;
 	
 	
--- serratecpub/src/main/java/org/serratec/serratecpub/model/Endereco.java
+++ serratecpub/src/main/java/org/serratec/serratecpub/model/Endereco.java
@@ -1,16 +1,39 @@
 package org.serratec.serratecpub.model;
 
+import org.serratec.serratecpub.util.TratamentoDeErro;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 @Entity
 public class Endereco {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	@NotBlank(message = TratamentoDeErro.NotBlankMessage)
+	@Size(min = 3, max = 15, message = TratamentoDeErro.SizeMessage)
 	private String cep;
+	@NotBlank(message = TratamentoDeErro.NotBlankMessage)
+	@Size(min = 3, max = 50, message = TratamentoDeErro.SizeMessage)
 	private String rua;
+	@NotBlank(message = TratamentoDeErro.NotBlankMessage)
+	@Size(min = 3, max = 50, message = TratamentoDeErro.SizeMessage)
 	private String bairro;
+	@NotBlank(message = TratamentoDeErro.NotBlankMessage)
+	@Size(min = 3, max = 50, message = TratamentoDeErro.SizeMessage)
 	private String cidade;
+	@NotBlank(message = TratamentoDeErro.NotBlankMessage)
+	@Size(min = 3, max = 10, message = TratamentoDeErro.SizeMessage)
 	private String numero;
+	@NotBlank(message = TratamentoDeErro.NotBlankMessage)
+	@Size(max = 50, message = TratamentoDeErro.SizeMessage)
 	private String complemento;
+	@NotBlank(message = TratamentoDeErro.NotBlankMessage)
+	@Size(min = 2, max = 2,message = TratamentoDeErro.SizeMessage)
 	private String uf;
 	
 	
@@ -18,6 +41,7 @@ public class Endereco {
 		return id;
 	}
 	public String getCep() {
+		//Fazer ligação com a api ViaCep
 		return cep;
 	}
 	public String getRua() {
