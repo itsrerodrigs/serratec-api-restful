b02a642a9acfb9f0ea09146c785682e87605ad6e
Lucas Coco <lucasgcocos@gmail.com>
2024-10-16 23:21:32 -0300

    Subindo o pacote TratamentoDeErro.java porque esqueci kkkkk

---
+++ serratecpub/src/main/java/org/serratec/serratecpub/util/TratamentoDeErro.java
@@ -0,0 +1,10 @@
+package org.serratec.serratecpub.util;
+
+//Aqui ficam todas as mensagens de tratamento de erro para
+//facilitar o código e evitar diversas String soltas
+
+public class TratamentoDeErro {
+	public static final String NotBlankMessage = "O valor não pode ser nulo ou estar em branco";
+	public static final String SizeMessage = "Quantidade minima ou maxima de caracteres não respeitada";
+	
+}
