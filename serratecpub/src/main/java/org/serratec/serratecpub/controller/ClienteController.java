1070bc92cdfc9328cea6c5b535a6d7ce81e86656
itsrerodrigs <itsrerodrigs@gmail.com>
2024-10-17 13:46:30 -0300

    add ClienteController e ClienteService

---
+++ serratecpub/src/main/java/org/serratec/serratecpub/controller/ClienteController.java
@@ -0,0 +1,69 @@
+package org.serratec.serratecpub.controller;
+
+import java.lang.StackWalker.Option;
+import java.util.List;
+import java.util.Optional;
+
+import org.serratec.serratecpub.dto.ClienteDto;
+import org.serratec.serratecpub.service.ClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/clientes")
+public class ClienteController {
+    @Autowired
+
+    private ClienteService clienteServico;
+
+    @GetMapping
+    public List<ClienteDto> listarClientes(){
+        return clienteServico.obterTodosClientes();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ClienteDto> listarClientePorId(@PathVariable Long id){
+        Optional<ClienteDto> dto = clienteServico.obterClientePorId(id);
+        if(dto.isPresent()){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(dto.get());
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ClienteDto cadastrarCliente(@RequestBody ClienteDto clienteDto){
+        return clienteServico.salvarCliente(clienteDto);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> excluirCliente(@PathVariable Long id){
+        if(!clienteServico.excluirCliente(id)){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Cliente não encontrado");
+        }
+        return ResponseEntity.ok("Cliente com id" +id+ " excluído com sucesso!");
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ClienteDto> alterarCliente(@PathVariable Long id,
+    @RequestBody ClienteDto clienteDto){
+        Optional<ClienteDto> clienteAlterado = clienteServico.alterarCliente(id, clienteDto);
+        if (!clienteAlterado.isPresent()){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(clienteAlterado.get());
+
+    }
+
+    
+}
