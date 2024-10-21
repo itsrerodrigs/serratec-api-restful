4a7ab8e624db376a0ff6fbe228f0542a782b296f
GustavoCS <gustatere1@gmail.com>
2024-10-21 15:18:46 -0300

    ajustes no pedido

--- serratecpub/src/main/java/org/serratec/serratecpub/repository/ProdutoRepository.java
+++ serratecpub/src/main/java/org/serratec/serratecpub/repository/ProdutoRepository.java
@@ -1,13 +1,13 @@
 package org.serratec.serratecpub.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.serratec.serratecpub.model.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import jakarta.persistence.Id;
-
 public interface ProdutoRepository extends JpaRepository<Produto, Long>{
 	
+	Optional<Produto> findByNomeIgnoreCase(String nome); 
 	List<Produto> findProdutoById(Long id);
 }
--- serratecpub/src/main/java/org/serratec/serratecpub/service/PedidoService.java
+++ serratecpub/src/main/java/org/serratec/serratecpub/service/PedidoService.java
@@ -4,12 +4,15 @@ import java.util.List;
 import java.util.Optional;
 
 import org.serratec.serratecpub.dto.PedidoDto;
+import org.serratec.serratecpub.dto.ProdutoDto;
 import org.serratec.serratecpub.model.Endereco;
 import org.serratec.serratecpub.model.ItemPedido;
 import org.serratec.serratecpub.model.Pedido;
+import org.serratec.serratecpub.model.Produto;
 import org.serratec.serratecpub.model.ViaCepService;
 import org.serratec.serratecpub.repository.EnderecoRepository;
 import org.serratec.serratecpub.repository.PedidoRepository;
+import org.serratec.serratecpub.repository.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,7 +24,8 @@ public class PedidoService {
 	private PedidoRepository pedidoRepositoriy;
 	@Autowired
 	private ViaCepService viaCepService;
-
+	@Autowired
+	private ProdutoRepository produtoRepository;
 	@Autowired
 	private EnderecoRepository enderecoRepository;
 
@@ -46,8 +50,9 @@ public class PedidoService {
 //	}
 	public PedidoDto salvarPedido(PedidoDto pedidoDto) {
 		Pedido pedidoEntity = pedidoDto.toEntity();
-
+			//verifica o endereco do cliente
 		if (pedidoDto.cliente().getEndereco() != null) {
+			// pega o cep do endereço e chama o metodo viacep para buscar as informações e preencher
 			Endereco endereco = viaCepService.preencherEnderecoComCep(pedidoDto.cliente().getEndereco().getCep());
 			if (endereco != null) {
 				endereco.setNumero(pedidoDto.cliente().getEndereco().getNumero());
@@ -58,19 +63,28 @@ public class PedidoService {
 				throw new IllegalArgumentException("CEP inválido ou sem retorno de dados");
 			}
 		}
-//		double vltd =0.0;
-//		for(ItemPedido ip : pedidoDto.itemPedido().get(0).calcularValorBruto()) {
-//			ip.setPedido(pedido);
-//			vltd += ip.getValorBruto();    		
-//			}
-//	
-//	pedido.setValorTotal(vltd);
-
+		for (ItemPedido item : pedidoEntity.getItemPedido()) {
+	        Produto produtoVerificado = verificarProduto(item.getProduto());
+	        item.setProduto(produtoVerificado); // Associar o produto verificado ao item
+	    }
+		
 		pedidoEntity = pedidoRepositoriy.save(pedidoEntity);
 		return PedidoDto.toDto(pedidoEntity);
 
 	}
-	
+	public Produto verificarProduto(Produto produto) {
+        //buscar o produto pelo nome
+        Optional<Produto> produtoExistente = produtoRepository.findByNomeIgnoreCase(produto.getNome());
+
+        // Se o produto existir, retorna ele
+        if (produtoExistente.isPresent()) {
+            return produtoExistente.get();
+        }
+
+        // Se não existir, cria e salva um novo produto
+        Produto novoProduto = produto;
+        return produtoRepository.save(novoProduto);
+    }
 	public boolean apagarPedido(Long id) {
 		if (!pedidoRepositoriy.existsById(id)) {
 			return false;
