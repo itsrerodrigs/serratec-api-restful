94cfd38a160f8b0ef10dcf04e1cd0753336d3ec7
itsrerodrigs <itsrerodrigs@gmail.com>
2024-10-22 16:44:50 -0300

    ajustes

--- serratecpub/src/main/java/org/serratec/serratecpub/repository/ClienteRepository.java
+++ serratecpub/src/main/java/org/serratec/serratecpub/repository/ClienteRepository.java
@@ -1,11 +1,7 @@
 package org.serratec.serratecpub.repository;
 
-import java.util.List;
-
 import org.serratec.serratecpub.model.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface ClienteRepository extends JpaRepository<Cliente, Long>{
 	
