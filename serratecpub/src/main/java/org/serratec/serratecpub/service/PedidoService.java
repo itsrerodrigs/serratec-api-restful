3e2ce1ca2e73f98ffdc7fba399391c3bfd5d6453
Lucas Coco <lucasgcocos@gmail.com>
2024-10-21 22:39:52 -0300

    DEIXANDO O PUT FUNCIONAL!!

--- serratecpub/src/main/java/org/serratec/serratecpub/service/PedidoService.java
+++ serratecpub/src/main/java/org/serratec/serratecpub/service/PedidoService.java
@@ -1,16 +1,18 @@
+package org.serratec.serratecpub.service;
 
+import java.util.List;
 import java.util.Optional;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.serratec.serratecpub.dto.ItemPedidoDto;
+
 import org.serratec.serratecpub.dto.PedidoDto;
-import org.serratec.serratecpub.dto.ProdutoDto;
+import org.serratec.serratecpub.dto.RelatorioPedidoDto;
 import org.serratec.serratecpub.model.ItemPedido;
 import org.serratec.serratecpub.model.Pedido;
 import org.serratec.serratecpub.model.Produto;
 import org.serratec.serratecpub.repository.PedidoRepository;
 import org.serratec.serratecpub.repository.ProdutoRepository;
 import org.serratec.serratecpub.util.EnderecoUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 @Service
 public class PedidoService {
@@ -23,38 +25,74 @@ public class PedidoService {
 
     @Autowired
     private EnderecoUtil enderecoUtil;
-
+    
+    
+	public List<PedidoDto> obterTodosPedidos() {
+        return pedidoRepository.findAll().stream().map(PedidoDto::toDto).toList();
+	}
+    public Optional<PedidoDto> obterPedidosPorId(Long id) {
+		if (!pedidoRepository.existsById(id)) {
+			return Optional.empty();
+		}
+		return Optional.of(PedidoDto.toDto(pedidoRepository.findById(id).get()));
+    }
+    public List<PedidoDto> obterPedidosPorNomeCliente(String nome) {
+    	return pedidoRepository.BuscarPedidoPorNomeCliente(nome).stream().map(PedidoDto::toDto).toList();
+    }
+    public Optional<RelatorioPedidoDto> obterRelatorioPedido(Long id) {
+		  Optional<Pedido> pedido = pedidoRepository.findById(id);
+		  if (pedido.isPresent()) {
+		    return Optional.of(RelatorioPedidoDto.toDto(pedido.get()));
+		  }
+		  return Optional.empty();
+	}
+    
     public PedidoDto salvarPedido(PedidoDto pedidoDto) {
         Pedido pedidoEntity = pedidoDto.toEntity();
         enderecoUtil.processarEndereco(pedidoDto.cliente(), pedidoEntity.getCliente());
         for (ItemPedido item : pedidoEntity.getItemPedido()) {
             Produto produtoVerificado = verificarProduto(item.getProduto());
             item.setProduto(produtoVerificado);
+            item.calcularValores();
         }
+        pedidoEntity.setValorTotal(pedidoDto.valorTd(pedidoEntity));
         pedidoEntity = pedidoRepository.save(pedidoEntity);
         return PedidoDto.toDto(pedidoEntity);
     }
 
-    public Produto verificarProduto(ProdutoDto produtoDto) {
-        Optional<Produto> produtoExistente = produtoRepository.findByNomeIgnoreCase(produtoDto.nome());
+    public Produto verificarProduto(Produto produto) {
+        Optional<Produto> produtoExistente = produtoRepository.findByNomeIgnoreCase(produto.getNome());
         if (produtoExistente.isPresent()) {
             return produtoExistente.get();
         }
-
-        Produto novoProduto = produtoDto.toEntity();
-        return produtoRepository.save(novoProduto);
+        return produtoRepository.save(produto);
     }
 
-    public Optional<PedidoDto> alterarPedido(Long id, PedidoDto pedidoDto) {
-        Optional<Pedido> pedidoExistente = pedidoRepository.findById(id);
-        if (pedidoExistente.isPresent()) {
-            Pedido pedido = pedidoDto.toEntity();
-            pedido.setId(id);
-            pedidoRepository.save(pedido);
-            return Optional.of(PedidoDto.toDto(pedido));
-        }
-        return Optional.empty();
-    }
+//    public Optional<PedidoDto> alterarPedido(Long id, PedidoDto pedidoDto) {
+//        Optional<Pedido> pedidoExistente = pedidoRepository.findById(id);
+//        if (pedidoExistente.isPresent()) {
+//            Pedido pedido = pedidoDto.toEntity();
+//            pedido.setId(id);
+//            pedidoRepository.save(pedido);
+//            return Optional.of(PedidoDto.toDto(pedido));
+//        }
+//        return Optional.empty();
+//    }
+	public Optional<PedidoDto> alterarPedido(Long id, PedidoDto pedidoDto) {
+		return pedidoRepository.findById(id).map(pedidoExistente -> {
+			Pedido pedido = pedidoDto.toEntity();
+			pedido.setId(id);
+			enderecoUtil.processarEndereco(pedidoDto.cliente(), pedido.getCliente());
+			for (ItemPedido item : pedido.getItemPedido()) {
+				Produto produtoVerificado = verificarProduto(item.getProduto());
+				item.setProduto(produtoVerificado);
+				item.calcularValores();
+			}
+			pedido.setValorTotal(pedidoDto.valorTd(pedido));
+			pedidoRepository.save(pedido);
+			return PedidoDto.toDto(pedido);
+		});
+	}
 
     public boolean apagarPedido(Long id) {
         if (pedidoRepository.existsById(id)) {
